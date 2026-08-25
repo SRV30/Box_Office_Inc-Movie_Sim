686628954052a4509ededd7c56f4a17ffb169bfb
Babin123456 <babin@example.com>
2026-08-26 01:03:01 +0530

    feat: complete crew team system for movie production (#529)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Directors from "./pages/directors/Directors";
 import Actors from "./pages/actors/Actors";
 import CrewMarket from "./pages/crew/CrewMarket";
 import OwnedCrew from "./pages/crew/OwnedCrew";
+import CrewManagementHub from "./pages/crew/CrewManagementHub";
 import ActiveMovies from "./pages/movies/ActiveMovies";
 import CreateMovie from "./pages/movies/CreateMovie";
 import MovieDetails from "./pages/movies/MovieDetails";
@@ -325,6 +326,14 @@ function App() {
           element={
             <ProtectedRoute>
               <CrewMarket />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/crew/hub"
+          element={
+            <ProtectedRoute>
+              <CrewManagementHub />
             </ProtectedRoute>
           }
         />
